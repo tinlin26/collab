73fb367d611f76045f53d98e55944a0cf7e70bee
Tina Lin <lin547661@gmail.com>
2023-07-14 10:38:55 -0400

    Initial Commit

---
+++ collab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  collab
+//
+//  Created by Tina Lin on 7/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, there!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ collab/collabApp.swift
@@ -0,0 +1,17 @@
+//
+//  collabApp.swift
+//  collab
+//
+//  Created by Tina Lin on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct collabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
